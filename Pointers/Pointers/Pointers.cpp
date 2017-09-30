243ab1e055ae0ffdf6c5b3301dd85cbf0bc0bf5d
martyhandicap11 <mcooney17.com>
2017-09-30 12:21:46 +0100

    Comment Correction

--- Pointers/Pointers/Pointers.cpp
+++ Pointers/Pointers/Pointers.cpp
@@ -22,7 +22,7 @@ int main()
 	ip  = &y;
 	printf("The value of is x %d\n", x); // Print value of x
 	printf("The value of is y %d\n", y); // Print value of y
-	printf("The value of is *ip %d\n", *ip);// Prints the value of variable x wich is 7
+	printf("The value of is *ip %d\n", *ip);// Prints the value of variable y wich is 42
 	
 	
 	return 0;
