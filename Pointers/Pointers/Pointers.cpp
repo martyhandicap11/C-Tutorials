df1e09b171324ec3328cb72059bad79ab206b67a
martyhandicap11 <mcooney17.com>
2017-09-30 11:57:15 +0100

    Pointers

---
+++ Pointers/Pointers/Pointers.cpp
@@ -0,0 +1,30 @@
+// Pointers.cpp : by Martin Cooney.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	int x = 7;
+	int y = 42;
+	int *ip = &x;
+
+	printf("The value of is x %d\n", x); // Print value of x
+	printf("The value of is y %d\n", y); // Print value of y
+	printf("The value of is *ip %d\n", *ip);// Prints the value of variable x wich is 7
+
+	x = 73;
+	printf("The value of is x %d\n", x); // Print value of x
+	printf("The value of is y %d\n", y); // Print value of y
+	printf("The value of is *ip %d\n", *ip);// Prints the value of variable x wich is 7
+
+	ip  = &y;
+	printf("The value of is x %d\n", x); // Print value of x
+	printf("The value of is y %d\n", y); // Print value of y
+	printf("The value of is *ip %d\n", *ip);// Prints the value of variable x wich is 7
+	
+	
+	return 0;
+}
+
